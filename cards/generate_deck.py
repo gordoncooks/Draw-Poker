ee89603e5573a2a4b02cf7610832189423e94e95
Gordon <cooksgordon22@gmail.com>
2025-05-07 23:04:17 +0200

    Add generate card deck functionality

---
+++ cards/generate_deck.py
@@ -0,0 +1,11 @@
+# This will generate and make the deck.
+cardsRank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+cardsSuit = ["H", "D", "C", "S"]
+
+def makeDeck():
+    deck = []
+    for cardSuit in cardsSuit:
+        for cardRank in cardsRank:
+            card = cardRank + cardSuit
+            deck.append(card)
+    return deck
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from draw-poker!")
+from cards import generate_deck
 
+def main():
+    deck = generate_deck.makeDeck()
+    
 
 if __name__ == "__main__":
     main()
