1231e8a87ea3208ece23fd97b3990443407a547a
Gordon <cooksgordon22@gmail.com>
2025-05-08 08:59:30 +0200

    Added logict to pass out cards to players

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from gameUI import ui
 def main():
     
     while(True):
+        cardDeckIndex = 0
         ui.clear_terminal()             # Always clear terminal incase for mess
         ui.displayMainMenu()            # Display main menu
         userInput = ui.userInputPromt() # Ask for user value
@@ -17,6 +18,22 @@ def main():
                 ui.clear_terminal()             # Clear terminal once game start
                 deck = generate_deck.makeDeck() # Create a fresh new card deck.
                 random.shuffle(deck)            # This shuffles the curent list and does not return a list
+
+                # Players:
+                players = [[],[],[],[],[]]
+
+                # Give cards to players
+                while(True):
+                    # Check last player total hands
+                    player_hand = players[4]
+                    hand_len = len(player_hand)
+                    if hand_len == 5:
+                        break
+                    else:
+                        # Give cards to players
+                        for player in players:
+                            player.append(deck[cardDeckIndex])
+                            cardDeckIndex += 1
 
             # Quit game if 2 was selected
             case "2":
